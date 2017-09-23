642bc896f2f0fc3f793a4868989e2250c508ee45
Carter Duncan <dukeblue2017@gmail.com>
2017-09-23 04:59:04 -0500

    Added the time and did some formatting, added an update button

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ $(document).ready(function(){
   var $body = $('body');
   $body.html('');
 
+  var u = $('<div class=update>Update</div>')
+  u.appendTo($body);
+
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
     var $tweet = $('<div class=tweet></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    var d = new Date
+    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + d);
     $tweet.appendTo($body);
     index -= 1;
   }
 
+  $('.tweet').on('mouseenter', function() {
+    $(this).addClass('highlight')
+  });
+
+  $('.tweet').on('mouseleave', function() {
+    $(this).removeClass('highlight')
+  });
+
+  $('.tweet').on('click', function() {
+    $(this).slideDown()
+  });
 
 });
